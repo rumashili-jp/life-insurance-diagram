a0b16687b012f16710caddd226b9fe0ea2fe2e6c
rumashili-jp <rumashili.jp@gmail.com>
2026-04-19 11:24:47 +0900

    updata

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 import { DrawUtil } from './utility.js';
 
+const pageWidth = 794;
+const pageHeight = 1123;
+const margin = 20;
+const axisMargin = 20;
+const innerMargin = 10;
+
 const canvas = document.getElementById('c');
 const draw = new DrawUtil(canvas);
 
 // 初期描画
-draw.rect(50, 50, 100, 60, 'skyblue');
-draw.line(50, 50, 150, 110, 'red', 2);
-draw.text('Hello!', 200, 100, 20);
-
-// クリックで描画
-canvas.addEventListener('click', (e) => {
-  const rect = canvas.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-
-  draw.circle?.(x, y, 5, 'black'); // circleあれば
-});
+const arrowX = [pageWidth-margin, pageHeight-(margin+axisMargin)]
+const arrowY = [margin+axisMargin, pageHeight-margin]
+const arrowW = axisMargin/2
+draw.line(margin+axisMargin, margin, arrowY[0], arrowY[1], 'black', 1);
+draw.line(arrowX[0], arrowX[1], arrowX[0]-arrowW, arrowX[1]-arrowW, 'black', 1)
+draw.line(arrowX[0], arrowX[1], arrowX[0]-arrowW, arrowX[1]+arrowW, 'black', 1)
+draw.line(margin, pageHeight-(margin+axisMargin), arrowX[0], arrowX[1], 'black', 1);
+draw.line(arrowY[0], arrowY[1], arrowY[0]-arrowW, arrowY[1]+arrowW, 'black', 1)
+draw.line(arrowY[0], arrowY[1], arrowY[0]+arrowW, arrowY[1]+arrowW, 'black', 1)
